305131dd8534ec819c6d94fba794c18daf205c26
thabomonamodi0 <thabomonamodi0@gmail.com>
2020-05-05 22:18:15 +0200

    Vanilla submission

--- src/main/java/JDBCConnect.java
+++ src/main/java/JDBCConnect.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class JDBCConnect
 {
@@ -12,22 +13,30 @@ public class JDBCConnect
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        String url = "jdbc:mysql://localhost:3306/umuzi?autoReconnect=true&useSSl=true";
+        String url = "jdbc:mysql://localhost:3306/umuzi/";
         String user = "root";
         String password = "zoba";
-
+        ArrayList sql = new ArrayList();
         {
-            tryg
+            try
             {
                 Connection con = DriverManager.getConnection(url,user,password);
                 Statement mystmt = con.createStatement();
-                String sql = "select * from umuzi.customer";
-                ResultSet result = mystmt.executeQuery(sql);
+                sql.add(1,"select * from umuzi.customer");
+                sql.add(2,"select Firstname from umuzi.customer");
+                sql.add(3,"select Firstname from umuzi.customer where CustomerID=1");
+                sql.add(4,"update umuzi.customer set Firstname='Lerato',Lastname='Mabisto' where CustomerID=1");
+                sql.add(5,"delete from umuzi.customer where CustomerID=2");
+                sql.add(6,"select count(distinct Status)from umuzi.orders");
+                sql.add(7,"select concat('R',max(Amount))as 'maximum payment'from umuzi.payment");
+                ResultSet result = mystmt.executeQuery(String.valueOf(sql));
 
                 while (result.next())
                 {
                     System.out.println(result.getString("Firstname"));
                 }
+                mystmt.close();
+                con.close();
             }
             catch (SQLException e)
             {
